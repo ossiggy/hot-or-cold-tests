b836a8fb9778eda17fae23f8aedd96913e514230
Ossiggy <mrossig201@gmail.com>
2017-11-09 20:30:30 -0500

    wrote feedback tests - all passing

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -14,5 +14,27 @@ describe('<Game />', () =>{
     instance.guess(wrongGuess);
     expect(wrapper.state('feedback')).toEqual('Please enter a valid number');
   });
-  
+  it('Produces correct feedback for each guess', () => {
+    const guess = '55';
+    const wrapper = shallow(<Game />);
+    const answer = wrapper.state('correctAnswer');
+    const difference = Math.abs(guess-answer);
+    const instance = wrapper.instance();
+    instance.guess(guess);
+    if (difference >= 50) {
+      expect(wrapper.state('feedback')).toEqual('You\'re Ice Cold...');
+    }
+    else if (difference >= 30) {
+      expect(wrapper.state('feedback')).toEqual('You\'re Cold...');
+    }
+    else if (difference >= 10) {
+      expect(wrapper.state('feedback')).toEqual('You\'re Warm');
+    }
+    else if (difference >= 1) {
+      expect(wrapper.state('feedback')).toEqual('You\'re Hot!');
+    }
+    else {
+      expect(wrapper.state('feedback')).toEqual('You got it!');
+    }
+  });
 });
