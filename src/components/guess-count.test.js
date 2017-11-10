d28f5e5d7793c252fbbf01f7f0d9b5416c37a1d6
Ossiggy <mrossig201@gmail.com>
2017-11-09 20:09:31 -0500

    wrote test suite for GuessCount component - all passing

---
+++ src/components/guess-count.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+
+import GuessCount from './guess-count';
+
+describe('<GuessCount />', () => {
+  it('Renders without crashing', () => {
+    shallow(<GuessCount />);
+  });
+
+  it('Displays the correct count', () => {
+    const count = 10;
+    const wrapper = shallow(<GuessCount count={count} />);
+    expect(wrapper.text()).toEqual('Guess #'+count+'!');
+  });
+});
